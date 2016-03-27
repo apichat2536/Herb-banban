23a610c75f99cea7e7c48fd4df9b932a10fe0270
apichat <apichatpayungwong602@gmail.com>
2016-03-27 11:52:45 +0700

    ได้ชิ้งโคไน้แร้ว

--- app/src/main/java/ubru/sabaipon/teerawat/apichat/herbbanban/MainActivity.java
+++ app/src/main/java/ubru/sabaipon/teerawat/apichat/herbbanban/MainActivity.java
@@ -2,9 +2,23 @@ package ubru.sabaipon.teerawat.apichat.herbbanban;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +40,123 @@ public class MainActivity extends AppCompatActivity {
 
         deleteSQLite();
 
+        //การชิ้งเจสัน
+
+        synJSON();
+
     }   // Main Method
+
+    private void synJSON() {
+
+        //คอนเนกโปธตคลอ
+
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        int intIndex = 0;
+        while (intIndex <= 1) {
+
+            //1 การคีเอสอินพุตสติม
+            InputStream inputStream = null;
+            String[] urlStrings = {"http://swiftcodingthai.com/herb/php_get_user.php",
+                    "http://swiftcodingthai.com/herb/php_get_herb.php"};
+
+            try {
+
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(urlStrings[intIndex]);
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                inputStream = httpEntity.getContent();
+
+
+            } catch (Exception e) {
+                Log.d("herd", "Error1 ==>" + e.toString());
+
+            }
+
+            //2 การคีเอส ชิ้งเจสัน
+            String strJSON = null;
+
+            try {
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                StringBuilder stringBuilder = new StringBuilder();
+                String strLine = null;
+
+                while ((strLine = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(strLine);
+                }
+                inputStream.close();
+                strJSON = stringBuilder.toString();
+
+
+            } catch (Exception e) {
+                Log.d("herd", "Error2 ==>" + e.toString());
+
+            }
+
+            //3 การอัปเดสเอสคิวเอลไล
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(strJSON);
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    switch (intIndex) {
+
+                        case 0:
+
+                            String strColumn1 = jsonObject.getString(MyManage.colum_User);
+                            String strColumn2 = jsonObject.getString(MyManage.colum_Password);
+                            String strColumn3 = jsonObject.getString(MyManage.colum_Status);
+                            String strColumn4 = jsonObject.getString(MyManage.colum_Name);
+                            String strColumn5 = jsonObject.getString(MyManage.colum_Surname);
+                            String strColumn6 = jsonObject.getString(MyManage.colum_Phone);
+                            String strColumn7 = jsonObject.getString(MyManage.colum_Address);
+
+                            myManage.addValueToSQLite(0, strColumn1, strColumn2, strColumn3,
+                                    strColumn4, strColumn5, strColumn6, strColumn7 );
+
+
+                            break;
+                        case 1:
+
+                            String strColumn8 = jsonObject.getString(MyManage.colum_Name);
+                            String strColumn9 = jsonObject.getString(MyManage.colum_Image);
+                            String strColumn10 = jsonObject.getString(MyManage.colum_Description);
+                            String strColumn11 = jsonObject.getString(MyManage.colum_HowTo);
+                            String strColumn12 = jsonObject.getString(MyManage.colum_Lat);
+                            String strColumn13 = jsonObject.getString(MyManage.colum_Lng);
+                            String strColumn14 = jsonObject.getString(MyManage.colum_Status);
+
+                            myManage.addValueToSQLite(1, strColumn8, strColumn9, strColumn10,
+                                    strColumn11, strColumn12, strColumn13, strColumn14 );
+
+                            break;
+
+
+                    }//switch
+
+
+                }//for
+
+
+            } catch (Exception e) {
+                Log.d("herd", "Error3 ==>" + e.toString());
+
+            }
+
+
+            intIndex += 1;
+        }//while
+
+
+        //ชิ้งเจสัน
+    }
 
     private void deleteSQLite() {
 
@@ -39,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void testAddValue() {
         int intTime = 0;
-        while (intTime<=1) {
+        while (intTime <= 1) {
 
             myManage.addValueToSQLite(intTime, "test1", "test2", "test3",
                     "test4", "test5", "test6", "test7");
